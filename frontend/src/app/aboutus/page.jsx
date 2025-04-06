cfeafd7405e908bd320436c45bccb80bd76bad73
shiprasingh08 <shiprasinghmau8@gmail.com>
2025-04-06 23:26:05 +0530

    ..

--- frontend/src/app/aboutus/page.jsx
+++ frontend/src/app/aboutus/page.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Aboutus = () => {
   return (
-    <div className='h-100vh w-full bg-pink-100 '>
-    <h1 className='text-center text-3xl font-extrabold mb-5 mt-10'>About us</h1>
-    <p className='ml-6 mr-6 text-center'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+    <div className='flex w-1/2 mt-10 h-full  bg-pink-100 '>
+    <h1 className=' text-center mb-5 mt-10 text-bold'>About us</h1>
+    <span>Our team</span>
+    <p className=' ml-6 mr-6 text-center'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
          Fugiat suscipit, voluptates quibusdam a libero veniam.
          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, repellat?
          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex qui quis amet. Nisi sapiente praesentium placeat.</p>
